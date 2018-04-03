7588c58ad94072b255f3be9063bcbb8c42ac7e2a
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-04-03 12:21:52 -0500

    Removed NtupleClass

--- Framework/src/SetUpTopTagger.cc
+++ Framework/src/SetUpTopTagger.cc
@@ -1,57 +1,50 @@
 #include "Framework/Framework/include/SetUpTopTagger.h"
-#include "Framework/Framework/include/NtupleClass.h"
 #include "SusyAnaTools/Tools/NTupleReader.h"
 
 #include <string>
 
-SetUpTopTagger::SetUpTopTagger(const NtupleClass& nTupleClass, 
-			       const std::vector<TLorentzVector>& hadtops, 
-			       const std::vector<std::vector<const TLorentzVector*>>& hadtopdaughters) : 
-    nTupleClass_               (nTupleClass),
-    tr_                        (nullptr),
-    AK4Inputs_                 (nullptr),
-    AK8Inputs_                 (nullptr),
-    Jets_                      (*nTupleClass_.Jets),
-    Jets_bDiscriminatorCSV_    (*nTupleClass_.Jets_bDiscriminatorCSV),
-    Jets_qgLikelihood_         (*nTupleClass_.Jets_qgLikelihood),
-    JetsAK8_                   (*nTupleClass_.JetsAK8),
-    JetsAK8_NsubjettinessTau1_ (*nTupleClass_.JetsAK8_NsubjettinessTau1),
-    JetsAK8_NsubjettinessTau2_ (*nTupleClass_.JetsAK8_NsubjettinessTau2),
-    JetsAK8_NsubjettinessTau3_ (*nTupleClass_.JetsAK8_NsubjettinessTau3),
-    JetsAK8_softDropMass_      (*nTupleClass_.JetsAK8_softDropMass),
-    JetsAK8_subjets_           (*nTupleClass_.JetsAK8_subjets),
-    Jets_ptD_                  (*nTupleClass_.Jets_ptD),
-    Jets_axismajor_            (*nTupleClass_.Jets_axismajor),
-    Jets_axisminor_            (*nTupleClass_.Jets_axisminor),
-    Jets_multiplicity_         (*nTupleClass_.Jets_multiplicity),
-    hadtops_                   (hadtops),
-    hadtopdaughters_           (hadtopdaughters)
-{
-}
-
 SetUpTopTagger::SetUpTopTagger(const NTupleReader& tr, 
 			       const std::vector<TLorentzVector>& hadtops, 
 			       const std::vector<std::vector<const TLorentzVector*>>& hadtopdaughters) : 
-    nTupleClass_               (nullptr),
     tr_                        (tr),
     AK4Inputs_                 (nullptr),
     AK8Inputs_                 (nullptr),
-    Jets_                      (tr_.getVec<TLorentzVector>("Jets")),
-    Jets_bDiscriminatorCSV_    (tr_.getVec<double>("Jets_bDiscriminatorCSV")),
-    Jets_qgLikelihood_         (tr_.getVec<double>("Jets_qgLikelihood")),
-    JetsAK8_                   (tr_.getVec<TLorentzVector>("JetsAK8")),
-    JetsAK8_NsubjettinessTau1_ (tr_.getVec<double>("JetsAK8_NsubjettinessTau1")),
-    JetsAK8_NsubjettinessTau2_ (tr_.getVec<double>("JetsAK8_NsubjettinessTau2")),
-    JetsAK8_NsubjettinessTau3_ (tr_.getVec<double>("JetsAK8_NsubjettinessTau3")),
-    JetsAK8_softDropMass_      (tr_.getVec<double>("JetsAK8_softDropMass")),
-    JetsAK8_subjets_           (tr_.getVec<std::vector<TLorentzVector>>("JetsAK8_subjets")),
-    Jets_ptD_                  (tr_.getVec<double>("Jets_ptD")),
-    Jets_axismajor_            (tr_.getVec<double>("Jets_axismajor")),
-    Jets_axisminor_            (tr_.getVec<double>("Jets_axisminor")),
-    Jets_multiplicity_         (tr_.getVec<int>("Jets_multiplicity")),
+    Jets_                      (tr.getVec<TLorentzVector>("Jets")),
+    Jets_bDiscriminatorCSV_    (tr.getVec<double>("Jets_bDiscriminatorCSV")),
+    Jets_qgLikelihood_         (tr.getVec<double>("Jets_qgLikelihood")),
+    JetsAK8_                   (tr.getVec<TLorentzVector>("JetsAK8")),
+    JetsAK8_NsubjettinessTau1_ (tr.getVec<double>("JetsAK8_NsubjettinessTau1")),
+    JetsAK8_NsubjettinessTau2_ (tr.getVec<double>("JetsAK8_NsubjettinessTau2")),
+    JetsAK8_NsubjettinessTau3_ (tr.getVec<double>("JetsAK8_NsubjettinessTau3")),
+    JetsAK8_softDropMass_      (tr.getVec<double>("JetsAK8_softDropMass")),
+    JetsAK8_subjets_           (tr.getVec<std::vector<TLorentzVector>>("JetsAK8_subjets")),
+    Jets_ptD_                  (tr.getVec<double>("Jets_ptD")),
+    Jets_axismajor_            (tr.getVec<double>("Jets_axismajor")),
+    Jets_axisminor_            (tr.getVec<double>("Jets_axisminor")),
+    Jets_multiplicity_         (tr.getVec<int>("Jets_multiplicity")),
     hadtops_                   (hadtops),
     hadtopdaughters_           (hadtopdaughters)
 {
+    // Use helper function to create input list 
+    // Create AK4 inputs object
+    AK4Inputs_ = new ttUtility::ConstAK4Inputs(
+                                               Jets_, 
+                                               Jets_bDiscriminatorCSV_,
+                                               Jets_qgLikelihood_, 
+                                               hadtops_, 
+                                               hadtopdaughters_);  
+    // Create AK8 inputs object
+    AK8Inputs_ = new ttUtility::ConstAK8Inputs(
+                                               JetsAK8_,
+                                               JetsAK8_NsubjettinessTau1_,
+                                               JetsAK8_NsubjettinessTau2_,
+                                               JetsAK8_NsubjettinessTau3_,
+                                               JetsAK8_softDropMass_,
+                                               JetsAK8_subjets_,
+                                               hadtops_,
+                                               hadtopdaughters_);  
+    //Add variables that are not passed to the constructor
+    addVariables();      
 }
 
 SetUpTopTagger::~SetUpTopTagger()
@@ -80,29 +73,7 @@ void SetUpTopTagger::addVariables()
 
 // Create jets constituents list combining AK4 and AK8 jets, these are used to construct top candiates
 // The vector of input constituents can also be constructed "by hand"    
-std::vector<Constituent> SetUpTopTagger::getConstituents()
+std::vector<Constituent> SetUpTopTagger::getConstituents() const
 {
-    // Use helper function to create input list 
-    // Create AK4 inputs object
-    AK4Inputs_ = new ttUtility::ConstAK4Inputs(
-                                               Jets_, 
-                                               Jets_bDiscriminatorCSV_,
-                                               Jets_qgLikelihood_, 
-                                               hadtops_, 
-                                               hadtopdaughters_);  
-    // Create AK8 inputs object
-    AK8Inputs_ = new ttUtility::ConstAK8Inputs(
-                                               JetsAK8_,
-                                               JetsAK8_NsubjettinessTau1_,
-                                               JetsAK8_NsubjettinessTau2_,
-                                               JetsAK8_NsubjettinessTau3_,
-                                               JetsAK8_softDropMass_,
-                                               JetsAK8_subjets_,
-                                               hadtops_,
-                                               hadtopdaughters_);  
-    //Add variables that are not passed to the constructor
-    addVariables();  
-
-    //Return Constituents
     return ttUtility::packageConstituents(*AK4Inputs_, *AK8Inputs_);
 }
