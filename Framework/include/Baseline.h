f0422adea053b47dff096d40bd6f92ab2e8d7569
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-04-13 14:04:53 -0500

    Added Baseline.h: for 0 lepton need to add others to it

---
+++ Framework/include/Baseline.h
@@ -0,0 +1,113 @@
+#ifndef BASELINE_H
+#define BASELINE_H
+
+class Baseline
+{
+private:
+    void baseline(NTupleReader& tr)
+    {
+
+        const auto& runtype      = tr.getVar<std::string>("runtype");     
+        const auto& filetag      = tr.getVar<std::string>("filetag");
+        const auto& TriggerNames = tr.getVec<std::string>("TriggerNames");
+        const auto& TriggerPass  = tr.getVec<int>("TriggerPass");
+        const auto& NGoodLeptons = tr.getVar<int>("NGoodLeptons");
+        const auto& NJets_pt45   = tr.getVar<int>("NJets_pt45");
+        const auto& HT_trigger   = tr.getVar<double>("HT_trigger");
+        const auto& NBJets_pt45  = tr.getVar<int>("NBJets_pt45");
+
+        // ------------------------
+        // -- MC dependent stuff
+        // -----------------------
+        bool passMadHT = true;
+        if(runtype == "MC"){
+            const auto& madHT  = tr.getVar<double>("madHT");
+            // Exclude events with MadGraph HT > 100 from the DY inclusive sample
+            if(filetag == "DYJetsToLL_M-50_Incl" && madHT > 100) passMadHT = false;
+        }
+        
+        // ------------------------------
+        // -- Data dependent stuff
+        // ------------------------------
+        
+        bool passTriggerAllHad   = PassTriggerAllHad(TriggerNames, TriggerPass);
+        bool passTriggerMuon     = PassTriggerMuon(TriggerNames, TriggerPass);
+        bool passTriggerElectron = PassTriggerElectron(TriggerNames, TriggerPass);
+        bool passTrigger = true;
+        if (runtype == "Data")
+        {
+            if (filetag == "Data_JetHT" && !passTriggerAllHad) passTrigger = false;
+            if (filetag == "Data_SingleMuon" && !passTriggerMuon) passTrigger = false;
+            if (filetag == "Data_SingleElectron" && !passTriggerElectron) passTrigger = false;
+        }
+        
+        // -------------------------------
+        // -- Define 0 Lepton Baseline
+        // -------------------------------
+        
+        bool passBaseline0l = passMadHT          &&
+                              passTrigger        &&
+                              NGoodLeptons == 0  && 
+                              NJets_pt45 >= 6    && 
+                              HT_trigger > 500   && 
+                              NBJets_pt45 >= 2;
+
+        tr.registerDerivedVar<bool>("passBaseline0l",passBaseline0l);
+
+    }
+
+    bool PassTriggerGeneral(std::vector<std::string> &mytriggers, const std::vector<std::string>& TriggerNames, const std::vector<int>& TriggerPass)
+    {
+        bool passTrigger = false;
+        for(unsigned int i=0; i<TriggerNames.size(); ++i)
+        {
+            if(TriggerPass.at(i) != 1)
+                continue;
+            std::string trigname = TriggerNames.at(i);
+            if( std::any_of(mytriggers.begin(), mytriggers.end(), [&] (std::string s) { return trigname.find(s)!=std::string::npos; }) )
+            {
+                passTrigger = true;
+                break;
+            }
+        }
+        return passTrigger;
+
+    }
+
+    bool PassTriggerAllHad(const std::vector<std::string>& TriggerNames, const std::vector<int>& TriggerPass)
+    {
+        std::vector<std::string> mytriggers {
+            //"HLT_PFHT1050", // 2017 trigger
+            //"HLT_PFHT900"
+            //"HLT_PFHT380_SixPFJet32_DoublePFBTagCSV", // 2017 trigger
+            //"HLT_PFHT430_SixPFJet40_PFBTagCSV", // 2017 trigger
+            "HLT_PFHT450_SixJet40_BTagCSV",
+                "HLT_PFHT400_SixJet30_DoubleBTagCSV",            
+                };
+        return PassTriggerGeneral(mytriggers,TriggerNames,TriggerPass);
+    }
+
+    bool PassTriggerMuon(const std::vector<std::string>& TriggerNames, const std::vector<int>& TriggerPass)
+    {
+        std::vector<std::string> mytriggers {"HLT_IsoMu24","HLT_IsoTkMu24_v"};
+        return PassTriggerGeneral(mytriggers,TriggerNames,TriggerPass);
+    }
+
+    bool PassTriggerElectron(const std::vector<std::string>& TriggerNames, const std::vector<int>& TriggerPass)
+    {
+        std::vector<std::string> mytriggers {"HLT_Ele27_WPTight_Gsf"};
+        return PassTriggerGeneral(mytriggers,TriggerNames,TriggerPass);
+    }
+
+public:
+    Baseline() 
+    {
+    }
+    
+    void operator()(NTupleReader& tr)
+    {
+        baseline(tr);
+    }
+};
+
+#endif
