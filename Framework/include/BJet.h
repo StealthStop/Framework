0caa45d81a5ea7e451eab331301a7d91bf13f8bb
Kelvin Mei <kmei@princeton.edu>
2018-05-25 11:14:20 -0500

    Added changes to Baseline and BJet

--- Framework/include/BJet.h
+++ Framework/include/BJet.h
@@ -6,45 +6,29 @@ class BJet
 private:
     std::vector<TLorentzVector>* bjets_;
     std::vector<TLorentzVector>* bjets_pt30_;
+    std::vector<TLorentzVector>* bjets_pt40_;
     std::vector<TLorentzVector>* bjets_pt45_;
 
     std::vector<TLorentzVector>* bjets_tight_;
     std::vector<TLorentzVector>* bjets_pt30_tight_;
     std::vector<TLorentzVector>* bjets_pt45_tight_;
     
-    std::vector<TLorentzVector>* bjets_clean_;
-    std::vector<TLorentzVector>* bjets_pt30_clean_;
-    std::vector<TLorentzVector>* bjets_pt45_clean_;
-
-    std::vector<TLorentzVector>* bjets_tight_clean_;
-    std::vector<TLorentzVector>* bjets_pt30_tight_clean_;
-    std::vector<TLorentzVector>* bjets_pt45_tight_clean_;
-
     void bjet(NTupleReader& tr)
     {
         const auto& Jets = tr.getVec<TLorentzVector>("Jets");
         const auto& Jets_bDiscriminatorCSV = tr.getVec<double>("Jets_bDiscriminatorCSV");
         const auto& etaCut = tr.getVar<double>("etaCut");
         const auto& JetsID = tr.getVec<bool>("Jets_ID");
-        const auto& cleanJets = tr.getVec<TLorentzVector>("Jetsclean");
-        const auto& cleanJetsCSV = tr.getVec<double>("Jetsclean_bDiscriminatorCSV");
-        const auto& cleanJetsID = tr.getVec<bool>("Jetsclean_ID");
         
         bjets_ = new std::vector<TLorentzVector>();
         bjets_pt30_ = new std::vector<TLorentzVector>();
+        bjets_pt40_ = new std::vector<TLorentzVector>();
         bjets_pt45_ = new std::vector<TLorentzVector>();
 
         bjets_tight_ = new std::vector<TLorentzVector>();
         bjets_pt30_tight_ = new std::vector<TLorentzVector>();
         bjets_pt45_tight_ = new std::vector<TLorentzVector>();
 
-        bjets_clean_ = new std::vector<TLorentzVector>();
-        bjets_pt30_clean_ = new std::vector<TLorentzVector>();
-        bjets_pt45_clean_ = new std::vector<TLorentzVector>();
-
-        bjets_tight_clean_ = new std::vector<TLorentzVector>();
-        bjets_pt30_tight_clean_ = new std::vector<TLorentzVector>();
-        bjets_pt45_tight_clean_ = new std::vector<TLorentzVector>();
 
         for (unsigned int ijet = 0; ijet < Jets.size(); ++ijet)
         {
@@ -59,6 +43,8 @@ private:
                 bjets_->push_back(lv); 
                 if( lv.Pt() > 30 )
                     bjets_pt30_->push_back(lv);
+                if( lv.Pt() > 40 )
+                    bjets_pt40_->push_back(lv);
                 if( lv.Pt() > 45 )
                     bjets_pt45_->push_back(lv);
             }
@@ -75,69 +61,13 @@ private:
             }
         }
 
-        if( cleanJets.size() != 0 ) {
-            for( unsigned int icjet = 0; icjet < cleanJets.size(); ++icjet ) 
-            {
-                if( !cleanJetsID.at(icjet) ) continue;
-                TLorentzVector lv( cleanJets.at(icjet) );
-            
-                if( abs( lv.Eta() ) < etaCut &&
-                    cleanJetsCSV.at(icjet) > 0.8484 
-                    )
-                {
-                    bjets_clean_->push_back(lv);
-                    if( lv.Pt() > 30 )
-                        bjets_pt30_clean_->push_back(lv);
-                    if( lv.Pt() > 45 )
-                        bjets_pt45_clean_->push_back(lv);
-                }
-
-                if( abs( lv.Eta() ) < etaCut &&
-                    cleanJetsCSV.at(icjet) > 0.9535
-                    )
-                {
-                    bjets_tight_clean_->push_back(lv);
-                    if( lv.Pt() > 30 )
-                        bjets_pt30_tight_clean_->push_back(lv);
-                    if( lv.Pt() > 45 )
-                        bjets_pt45_tight_clean_->push_back(lv);
-                }
-            }
-        }
-        else if( cleanJets.size() == 0 ) {
-            for( unsigned int icjet = 0; icjet < Jets.size(); ++icjet ) 
-            {
-                if( !JetsID.at(icjet) ) continue;
-                TLorentzVector lv( Jets.at(icjet) );
-            
-                if( abs( lv.Eta() ) < etaCut &&
-                    Jets_bDiscriminatorCSV.at(icjet) > 0.8484 
-                    )
-                {
-                    bjets_clean_->push_back(lv);
-                    if( lv.Pt() > 30 )
-                        bjets_pt30_clean_->push_back(lv);
-                    if( lv.Pt() > 45 )
-                        bjets_pt45_clean_->push_back(lv);
-                }
-
-                if( abs( lv.Eta() ) < etaCut &&
-                    Jets_bDiscriminatorCSV.at(icjet) > 0.9535
-                    )
-                {
-                    bjets_tight_clean_->push_back(lv);
-                    if( lv.Pt() > 30 )
-                        bjets_pt30_tight_clean_->push_back(lv);
-                    if( lv.Pt() > 45 )
-                        bjets_pt45_tight_clean_->push_back(lv);
-                }
-            }   
-        }
 
         tr.registerDerivedVec("BJets",        bjets_);
         tr.registerDerivedVar("NBJets",      (bjets_==nullptr)?0:bjets_->size());
         tr.registerDerivedVec("BJets_pt30",   bjets_pt30_);
         tr.registerDerivedVar("NBJets_pt30", (bjets_pt30_==nullptr)?0:bjets_pt30_->size());
+        tr.registerDerivedVec("BJets_pt40",   bjets_pt40_);
+        tr.registerDerivedVar("NBJets_pt40", (bjets_pt40_==nullptr)?0:bjets_pt40_->size());
         tr.registerDerivedVec("BJets_pt45",   bjets_pt45_);
         tr.registerDerivedVar("NBJets_pt45", (bjets_pt45_==nullptr)?0:bjets_pt45_->size());
 
@@ -148,35 +78,17 @@ private:
         tr.registerDerivedVec("BJets_pt45_tight",   bjets_pt45_tight_);
         tr.registerDerivedVar("NBJets_pt45_tight", (bjets_pt45_tight_==nullptr)?0:bjets_pt45_tight_->size());
 
-        tr.registerDerivedVec("BJets_clean",        bjets_clean_);
-        tr.registerDerivedVar("NBJets_clean",      (bjets_clean_==nullptr)?0:bjets_clean_->size());
-        tr.registerDerivedVec("BJets_pt30_clean",   bjets_pt30_clean_);
-        tr.registerDerivedVar("NBJets_pt30_clean", (bjets_pt30_clean_==nullptr)?0:bjets_pt30_clean_->size());
-        tr.registerDerivedVec("BJets_pt45_clean",   bjets_pt45_clean_);
-        tr.registerDerivedVar("NBJets_pt45_clean", (bjets_pt45_clean_==nullptr)?0:bjets_pt45_clean_->size());
-
-        tr.registerDerivedVec("BJets_tight_clean",          bjets_tight_clean_);
-        tr.registerDerivedVar("NBJets_tight_clean",        (bjets_tight_clean_==nullptr)?0:bjets_tight_clean_->size());
-        tr.registerDerivedVec("BJets_pt30_tight_clean",     bjets_pt30_tight_clean_);
-        tr.registerDerivedVar("NBJets_pt30_tight_clean",   (bjets_pt30_tight_clean_==nullptr)?0:bjets_pt30_tight_clean_->size());
-        tr.registerDerivedVec("BJets_pt45_tight_clean",     bjets_pt45_tight_clean_);
-        tr.registerDerivedVar("NBJets_pt45_tight_clean",   (bjets_pt45_tight_clean_==nullptr)?0:bjets_pt45_tight_clean_->size());
     }
 
 public:
     BJet() 
         : bjets_(nullptr)
         , bjets_pt30_(nullptr)
+        , bjets_pt40_(nullptr)
         , bjets_pt45_(nullptr)
         , bjets_tight_(nullptr)
         , bjets_pt30_tight_(nullptr)
         , bjets_pt45_tight_(nullptr)
-        , bjets_clean_(nullptr)
-        , bjets_pt30_clean_(nullptr)
-        , bjets_pt45_clean_(nullptr)
-        , bjets_tight_clean_(nullptr)
-        , bjets_pt30_tight_clean_(nullptr)
-        , bjets_pt45_tight_clean_(nullptr)
     {}
 
     void operator()(NTupleReader& tr)
--- Framework/include/Baseline.h
+++ Framework/include/Baseline.h
@@ -15,17 +15,12 @@ private:
         const auto& NGoodMuons          = tr.getVar<int>("NGoodMuons");
         const auto& NGoodElectrons      = tr.getVar<int>("NGoodElectrons");
         const auto& NJets_pt45          = tr.getVar<int>("NJets_pt45");
-        const auto& NJets_pt45_clean    = tr.getVar<int>("NJets_pt45_clean");
         const auto& HT_trigger          = tr.getVar<double>("HT_trigger");
         const auto& NBJets_pt45         = tr.getVar<int>("NBJets_pt45");
-        const auto& NBJets_pt45_clean   = tr.getVar<int>("NBJets_pt45_clean");
         const auto& NBJets_pt30         = tr.getVar<int>("NBJets_pt30"); 
-        const auto& NBJets_pt30_clean   = tr.getVar<int>("NBJets_pt30_clean"); 
         const auto& NJets_pt30          = tr.getVar<int>("NJets_pt30"); 
-        const auto& NJets_pt30_clean    = tr.getVar<int>("NJets_pt30_clean"); 
         const auto& onZ                 = tr.getVar<bool>("onZ"); 
         const auto& JetID               = tr.getVar<bool>("JetID"); 
-        const auto& JetID_clean         = tr.getVar<bool>("JetIDclean"); 
 
         // ------------------------
         // -- MC dependent stuff
@@ -72,14 +67,14 @@ private:
                               HT_trigger > 500   && 
                               NBJets_pt45 >= 2;
         
-        bool passBaseline0l_clean = JetID_clean  &&
+        bool passBaseline0l_hadTrig = JetID              &&
                               passMadHT          &&
                               passTrigger        &&
                               passBlindHad       &&
-                              NGoodLeptons == 0  && 
-                              NJets_pt45_clean >= 6    && 
+                              NJets_pt45 >= 6    && 
                               HT_trigger > 500   && 
-                              NBJets_pt45_clean >= 2;
+                              NBJets_pt45 >= 2;
+        
 
         // -------------------------------
         // -- Define 1 Lepton Baseline
@@ -108,32 +103,6 @@ private:
         bool passBaseline1l = passBaseline1mu || passBaseline1el;
         
         // -------------------------------
-        // -- Define 1 Lepton Baseline with Clean Jets
-        // -------------------------------
-        
-        bool passBaseline1mu_clean =    JetID_clean         &&
-                                        passMadHT           &&
-                                        passTrigger         &&
-                                        (runtype != "Data" || filetag == "Data_SingleMuon") &&
-                                        passBlindLep        &&
-                                        NGoodMuons == 1     && 
-                                        NGoodElectrons == 0 &&
-                                        NJets_pt30_clean >= 6 && 
-                                        NBJets_pt30_clean >= 1;
-
-        bool passBaseline1el_clean =    JetID_clean         &&
-                                        passMadHT           &&
-                                        passTrigger         &&
-                                        (runtype != "Data" || filetag == "Data_SingleElectron") &&
-                                        passBlindLep        &&
-                                        NGoodElectrons == 1 &&
-                                        NGoodMuons == 0     &&
-                                        NJets_pt30_clean >= 6     && 
-                                        NBJets_pt30_clean >= 1;
-
-        bool passBaseline1l_clean = passBaseline1mu_clean || passBaseline1el_clean;
-        
-        // -------------------------------
         // -- Define 1 Lepton Baseline with no Jet ID
         // -------------------------------
         
@@ -159,32 +128,6 @@ private:
 
         bool passBaseline1l_noID = passBaseline1mu_noID || passBaseline1el_noID;
         
-        // -------------------------------
-        // -- Define 1 Lepton Baseline with Clean Jets and no Jet ID
-        // -------------------------------
-        
-        bool passBaseline1mu_noID_clean = 
-                                        passMadHT           &&
-                                        passTrigger         &&
-                                        (runtype != "Data" || filetag == "Data_SingleMuon") &&
-                                        passBlindLep        &&
-                                        NGoodMuons == 1     && 
-                                        NGoodElectrons == 0 &&
-                                        NJets_pt30_clean >= 6 && 
-                                        NBJets_pt30_clean >= 1;
-
-        bool passBaseline1el_noID_clean = 
-                                        passMadHT           &&
-                                        passTrigger         &&
-                                        (runtype != "Data" || filetag == "Data_SingleElectron") &&
-                                        passBlindLep        &&
-                                        NGoodElectrons == 1 &&
-                                        NGoodMuons == 0     &&
-                                        NJets_pt30_clean >= 6     && 
-                                        NBJets_pt30_clean >= 1;
-
-        bool passBaseline1l_noID_clean = passBaseline1mu_noID_clean || passBaseline1el_noID_clean;
-
         // ----------------------------------
         // -- Define 2 Lepton onZ Baseline
         // ----------------------------------
@@ -199,19 +142,6 @@ private:
                                  NJets_pt30 >= 6; 
         
         // ----------------------------------
-        // -- Define 2 Lepton onZ Baseline with Clean Jets
-        // ----------------------------------
-        
-        bool passBaseline2lonZ_clean =  JetID_clean              &&
-                                        passMadHT          &&
-                                        passTrigger        &&
-                                        NGoodLeptons == 2  && 
-                                        onZ                &&
-                                        (runtype != "Data" || (NGoodMuons == 2 && filetag == "Data_SingleMuon" ) 
-                                                           || (NGoodElectrons == 2 && filetag == "Data_SingleElectron") ) &&
-                                        NJets_pt30_clean >= 6; 
-        
-        // ----------------------------------
         // -- Define 2 Lepton onZ Baseline with no JetID
         // ----------------------------------
         
@@ -223,18 +153,6 @@ private:
                                                                || (NGoodElectrons == 2 && filetag == "Data_SingleElectron") ) &&
                                             NJets_pt30 >= 6; 
         
-        // ----------------------------------
-        // -- Define 2 Lepton onZ Baseline with Clean Jets with no JetID
-        // ----------------------------------
-        
-        bool passBaseline2lonZ_noID_clean =     passMadHT          &&
-                                                passTrigger        &&
-                                                NGoodLeptons == 2  && 
-                                                onZ                &&
-                                                (runtype != "Data" || (NGoodMuons == 2 && filetag == "Data_SingleMuon" ) 
-                                                                   || (NGoodElectrons == 2 && filetag == "Data_SingleElectron") ) &&
-                                                NJets_pt30_clean >= 6; 
-
 
         // -----------------------------------
         // -- Define 2 Lepton offZ Baseline
@@ -251,31 +169,16 @@ private:
                               NJets_pt30 >= 6    && 
                               NBJets_pt30 >= 1;
         
-        bool passBaseline2l_clean = JetID_clean  &&
-                              passMadHT          &&
-                              passTrigger        &&
-                              passBlindLep       &&
-                              NGoodLeptons == 2  && 
-                              !onZ               &&
-                              (runtype != "Data" || (NGoodMuons >= 1 && filetag == "Data_SingleMuon" ) 
-                                                 || (NGoodElectrons == 2 && filetag == "Data_SingleElectron") ) &&
-                              NJets_pt30_clean >= 6    && 
-                              NBJets_pt30_clean >= 1;
 
         tr.registerDerivedVar<bool>("passBaseline0l",passBaseline0l);
-        tr.registerDerivedVar<bool>("passBaseline0l_clean",passBaseline0l_clean);
+        tr.registerDerivedVar<bool>("passBaseline0l_hadTrig", passBaseline0l_hadTrig);
         tr.registerDerivedVar<bool>("passBaseline1l",passBaseline1l);
-        tr.registerDerivedVar<bool>("passBaseline1l_clean",passBaseline1l_clean);
         tr.registerDerivedVar<bool>("passBaseline1l_noID",passBaseline1l_noID);
-        tr.registerDerivedVar<bool>("passBaseline1l_noID_clean",passBaseline1l_noID_clean);
         tr.registerDerivedVar<bool>("passBaseline1mu",passBaseline1mu);
         tr.registerDerivedVar<bool>("passBaseline1el",passBaseline1el);
         tr.registerDerivedVar<bool>("passBaseline2lonZ",passBaseline2lonZ);
-        tr.registerDerivedVar<bool>("passBaseline2lonZ_clean",passBaseline2lonZ_clean);
         tr.registerDerivedVar<bool>("passBaseline2lonZ_noID",passBaseline2lonZ_noID);
-        tr.registerDerivedVar<bool>("passBaseline2lonZ_noID_clean",passBaseline2lonZ_noID_clean);
         tr.registerDerivedVar<bool>("passBaseline2l",passBaseline2l);
-        tr.registerDerivedVar<bool>("passBaseline2l_clean",passBaseline2l_clean);
         tr.registerDerivedVar<bool>("passBlindHad",passBlindHad);
         tr.registerDerivedVar<bool>("passBlindLep",passBlindLep);
         tr.registerDerivedVar<bool>("passTrigger",passTrigger);
