50f024728a0fa40f313fb1beceba1366a102c0b4
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-08-23 18:27:16 -0500

    Added more var for deepESM

--- Framework/include/DeepEventShape.h
+++ Framework/include/DeepEventShape.h
@@ -15,12 +15,20 @@ private:
     float* basePtr_;
     int len_;
 
-    int fwm2_top6_, fwm3_top6_, fwm4_top6_, fwm5_top6_, fwm6_top6_, fwm7_top6_, fwm8_top6_, fwm9_top6_, fwm10_top6_, jmt_ev0_top6_, jmt_ev1_top6_, jmt_ev2_top6_, NGoodJets_double_;
+    int fwm2_top6_, fwm3_top6_, fwm4_top6_, fwm5_top6_, fwm6_top6_, fwm7_top6_, fwm8_top6_, fwm9_top6_, fwm10_top6_, jmt_ev0_top6_, jmt_ev1_top6_, jmt_ev2_top6_;
+    int NGoodJets_double_;
+    int Jet_pt_1_, Jet_pt_2_, Jet_pt_3_, Jet_pt_4_, Jet_pt_5_, Jet_pt_6_, Jet_eta_1_, Jet_eta_2_, Jet_eta_3_, Jet_eta_4_, Jet_eta_5_, Jet_eta_6_;
+    int Jet_phi_1_, Jet_phi_2_, Jet_phi_3_, Jet_phi_4_, Jet_phi_5_, Jet_phi_6_, Jet_m_1_, Jet_m_2_, Jet_m_3_, Jet_m_4_, Jet_m_5_, Jet_m_6_; 
+    int BestComboAvgMass_;
 
 public:
     EventShapeCalculator()
     {
-        fwm2_top6_ = fwm3_top6_ = fwm4_top6_ = fwm5_top6_ = fwm6_top6_ = fwm7_top6_ = fwm8_top6_ = fwm9_top6_ = fwm10_top6_ = jmt_ev0_top6_ = jmt_ev1_top6_ = jmt_ev2_top6_ = NGoodJets_double_ = -1;
+        fwm2_top6_ = fwm3_top6_ = fwm4_top6_ = fwm5_top6_ = fwm6_top6_ = fwm7_top6_ = fwm8_top6_ = fwm9_top6_ = fwm10_top6_ = jmt_ev0_top6_ = jmt_ev1_top6_ = jmt_ev2_top6_ = -1; 
+        NGoodJets_double_ = -1;
+        Jet_pt_1_ = Jet_pt_2_ = Jet_pt_3_ = Jet_pt_4_ = Jet_pt_5_ = Jet_pt_6_ = Jet_eta_1_ = Jet_eta_2_ = Jet_eta_3_ = Jet_eta_4_ = Jet_eta_5_ = Jet_eta_6_ = -1;
+        Jet_phi_1_ = Jet_phi_2_ = Jet_phi_3_ = Jet_phi_4_ = Jet_phi_5_ = Jet_phi_6_ = Jet_m_1_ = Jet_m_2_ = Jet_m_3_ = Jet_m_4_ = Jet_m_5_ = Jet_m_6_ = -1; 
+        BestComboAvgMass_ = -1;
     }
 
     /**
@@ -45,6 +53,31 @@ public:
             else if(vars[i].compare("jmt_ev1_top6") == 0) jmt_ev1_top6_ = i;
             else if(vars[i].compare("jmt_ev2_top6") == 0) jmt_ev2_top6_ = i;
             else if(vars[i].compare("NGoodJets_double") == 0) NGoodJets_double_ = i;
+            else if(vars[i].compare("Jet_pt_1") == 0) Jet_pt_1_ = i;
+            else if(vars[i].compare("Jet_pt_2") == 0) Jet_pt_2_ = i;
+            else if(vars[i].compare("Jet_pt_3") == 0) Jet_pt_3_ = i;
+            else if(vars[i].compare("Jet_pt_4") == 0) Jet_pt_4_ = i;
+            else if(vars[i].compare("Jet_pt_5") == 0) Jet_pt_5_ = i;
+            else if(vars[i].compare("Jet_pt_6") == 0) Jet_pt_6_ = i;
+            else if(vars[i].compare("Jet_eta_1") == 0) Jet_eta_1_ = i;
+            else if(vars[i].compare("Jet_eta_2") == 0) Jet_eta_2_ = i;
+            else if(vars[i].compare("Jet_eta_3") == 0) Jet_eta_3_ = i;
+            else if(vars[i].compare("Jet_eta_4") == 0) Jet_eta_4_ = i;
+            else if(vars[i].compare("Jet_eta_5") == 0) Jet_eta_5_ = i;
+            else if(vars[i].compare("Jet_eta_6") == 0) Jet_eta_6_ = i;
+            else if(vars[i].compare("Jet_phi_1") == 0) Jet_phi_1_ = i;
+            else if(vars[i].compare("Jet_phi_2") == 0) Jet_phi_2_ = i;
+            else if(vars[i].compare("Jet_phi_3") == 0) Jet_phi_3_ = i;
+            else if(vars[i].compare("Jet_phi_4") == 0) Jet_phi_4_ = i;
+            else if(vars[i].compare("Jet_phi_5") == 0) Jet_phi_5_ = i;
+            else if(vars[i].compare("Jet_phi_6") == 0) Jet_phi_6_ = i;
+            else if(vars[i].compare("Jet_m_1") == 0) Jet_m_1_ = i;
+            else if(vars[i].compare("Jet_m_2") == 0) Jet_m_2_ = i;
+            else if(vars[i].compare("Jet_m_3") == 0) Jet_m_3_ = i;
+            else if(vars[i].compare("Jet_m_4") == 0) Jet_m_4_ = i;
+            else if(vars[i].compare("Jet_m_5") == 0) Jet_m_5_ = i;
+            else if(vars[i].compare("Jet_m_6") == 0) Jet_m_6_ = i;
+            else if(vars[i].compare("BestComboAvgMass") == 0) BestComboAvgMass_ = i;
         }
     }
     /**
@@ -69,6 +102,31 @@ public:
         if(jmt_ev1_top6_ >= 0) *(basePtr_ + jmt_ev1_top6_ + len_*iCand) =  tr.getVar<double>("jmt_ev1_top6");
         if(jmt_ev2_top6_ >= 0) *(basePtr_ + jmt_ev2_top6_ + len_*iCand) =  tr.getVar<double>("jmt_ev2_top6");
         if(NGoodJets_double_ >= 0) *(basePtr_ + NGoodJets_double_ + len_*iCand) =  static_cast<double>(tr.getVar<unsigned long>("NGoodJets"));
+        if(Jet_pt_1_ >= 0) *(basePtr_ + Jet_pt_1_ + len_*iCand) = tr.getVar<double>("Jet_pt_1");
+        if(Jet_pt_2_ >= 0) *(basePtr_ + Jet_pt_2_ + len_*iCand) = tr.getVar<double>("Jet_pt_2");
+        if(Jet_pt_3_ >= 0) *(basePtr_ + Jet_pt_3_ + len_*iCand) = tr.getVar<double>("Jet_pt_3");
+        if(Jet_pt_4_ >= 0) *(basePtr_ + Jet_pt_4_ + len_*iCand) = tr.getVar<double>("Jet_pt_4");
+        if(Jet_pt_5_ >= 0) *(basePtr_ + Jet_pt_5_ + len_*iCand) = tr.getVar<double>("Jet_pt_5");
+        if(Jet_pt_6_ >= 0) *(basePtr_ + Jet_pt_6_ + len_*iCand) = tr.getVar<double>("Jet_pt_6");
+        if(Jet_eta_1_ >= 0) *(basePtr_ + Jet_eta_1_ + len_*iCand) = tr.getVar<double>("Jet_eta_1");
+        if(Jet_eta_2_ >= 0) *(basePtr_ + Jet_eta_2_ + len_*iCand) = tr.getVar<double>("Jet_eta_2");
+        if(Jet_eta_3_ >= 0) *(basePtr_ + Jet_eta_3_ + len_*iCand) = tr.getVar<double>("Jet_eta_3");
+        if(Jet_eta_4_ >= 0) *(basePtr_ + Jet_eta_4_ + len_*iCand) = tr.getVar<double>("Jet_eta_4");
+        if(Jet_eta_5_ >= 0) *(basePtr_ + Jet_eta_5_ + len_*iCand) = tr.getVar<double>("Jet_eta_5");
+        if(Jet_eta_6_ >= 0) *(basePtr_ + Jet_eta_6_ + len_*iCand) = tr.getVar<double>("Jet_eta_6");
+        if(Jet_phi_1_ >= 0) *(basePtr_ + Jet_phi_1_ + len_*iCand) = tr.getVar<double>("Jet_phi_1");
+        if(Jet_phi_2_ >= 0) *(basePtr_ + Jet_phi_2_ + len_*iCand) = tr.getVar<double>("Jet_phi_2");
+        if(Jet_phi_3_ >= 0) *(basePtr_ + Jet_phi_3_ + len_*iCand) = tr.getVar<double>("Jet_phi_3");
+        if(Jet_phi_4_ >= 0) *(basePtr_ + Jet_phi_4_ + len_*iCand) = tr.getVar<double>("Jet_phi_4");
+        if(Jet_phi_5_ >= 0) *(basePtr_ + Jet_phi_5_ + len_*iCand) = tr.getVar<double>("Jet_phi_5");
+        if(Jet_phi_6_ >= 0) *(basePtr_ + Jet_phi_6_ + len_*iCand) = tr.getVar<double>("Jet_phi_6");
+        if(Jet_m_1_ >= 0) *(basePtr_ + Jet_m_1_ + len_*iCand) = tr.getVar<double>("Jet_m_1");
+        if(Jet_m_2_ >= 0) *(basePtr_ + Jet_m_2_ + len_*iCand) = tr.getVar<double>("Jet_m_2");
+        if(Jet_m_3_ >= 0) *(basePtr_ + Jet_m_3_ + len_*iCand) = tr.getVar<double>("Jet_m_3");
+        if(Jet_m_4_ >= 0) *(basePtr_ + Jet_m_4_ + len_*iCand) = tr.getVar<double>("Jet_m_4");
+        if(Jet_m_5_ >= 0) *(basePtr_ + Jet_m_5_ + len_*iCand) = tr.getVar<double>("Jet_m_5");
+        if(Jet_m_6_ >= 0) *(basePtr_ + Jet_m_6_ + len_*iCand) = tr.getVar<double>("Jet_m_6");
+        if(BestComboAvgMass_ >= 0) *(basePtr_ + BestComboAvgMass_ + len_*iCand) = tr.getVar<double>("BestComboAvgMass");
     }
 };
 
@@ -115,7 +173,7 @@ private:
                 keepLooping = true;
         
                 vars_.push_back(varName);
-                //std::cout<<varName<<std::endl;
+                std::cout<<varName<<std::endl;
             }
             ++iVar;
         }
@@ -200,7 +258,7 @@ private:
         
         //discriminators is a 2D array, we only want the first entry of every array
         double discriminator = static_cast<double>(discriminators[iCand*TF_Dim(output_values[0], 1)]);
-        
+
         for(auto tensor : input_values)  TF_DeleteTensor(tensor);
         for(auto tensor : output_values) TF_DeleteTensor(tensor);
         
