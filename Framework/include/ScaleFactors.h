c1cae4172d8286ee627d82b75df3e6b063e6df56
Kelvin Mei <kmei@princeton.edu>
2018-09-03 10:46:56 -0500

    Added all lepton scale factors

--- Framework/include/ScaleFactors.h
+++ Framework/include/ScaleFactors.h
@@ -1,9 +1,14 @@
 #ifndef SCALEFACTORS_H
 #define SCALEFACTORS_H
 
+#include "TGraph.h"
 class ScaleFactors
 {
 private:
+
+    std::string eleSFRootFileName_;
+    std::string muSFRootFileName_;
+
     void scaleFactors(NTupleReader& tr)
     {
         // Get needed branches
@@ -86,11 +91,158 @@ private:
         tr.registerDerivedVar( "PDFweightUp",   NNPDF_from_median_up );
         tr.registerDerivedVar( "PDFweightDown", NNPDF_from_median_down );
         tr.registerDerivedVar( "PDFweightNom",  central );
+        
+        //Adding code for implementing electron scale factors
+        const auto& electrons       = tr.getVec<TLorentzVector>("Electrons");
+        const auto& goodElectrons   = tr.getVec<bool>("GoodElectrons");
+        
+        auto* electronSFs           = new std::vector<double>;
+        double leadingElectronSF = -1.0;
 
+        TH1::AddDirectory(false); //According to Joe, this is a magic incantation that lets the root file close - if this is not here, there are segfaults?
+
+        TFile eleSFRootFile( muSFRootFileName_.c_str() );
+        
+        TH2F *eleSFHistoTight = (TH2F*)eleSFRootFile.Get("GsfElectronToCutBasedSpring15T");
+        TH2F *eleSFHistoIso   = (TH2F*)eleSFRootFile.Get("MVAVLooseElectronToMini");
+        TH2F *eleSFHistoReco  = (TH2F*)eleSFRootFile.Get("EGamma_SF2D");
+
+        //Loop through the electrons
+        for( unsigned int iel = 0; iel < electrons.size(); iel++ ) {
+            //If it is not a good lepton, give scale factor of 1.0
+            if( !goodElectrons.at(iel) ) electronSFs->push_back(1.0);
+
+            else {
+                //Get the scale factor from the rootfile
+                double elpt     = electrons.at(iel).Pt();
+                double eleta    = electrons.at(iel).Eta();
+                int xbin = 0, ybin = 0, ptbin = 0, etabin = 0;
+
+                for( unsigned int ixbin = 0; ixbin < eleSFHistoTight->GetNbinsX()+1; ixbin++ ) {
+                    double tempxBinEdgeMax = (double) eleSFHistoTight->GetXaxis()->GetBinUpEdge(ixbin);
+                    if( elpt < tempxBinEdgeMax )  {
+                        xbin = ixbin; 
+                        break;
+                    }
+                }
+
+                for( unsigned int iybin = 0; iybin < eleSFHistoTight->GetNbinsY()+1; iybin++ ) {
+                    double tempyBinEdgeMax = (double)eleSFHistoTight->GetYaxis()->GetBinUpEdge(iybin);
+                    if( std::fabs(eleta) < tempyBinEdgeMax ) {
+                        ybin = iybin;
+                        break;
+                    }
+                }
+                
+                if( xbin == 0 && elpt > 200.0) xbin = eleSFHistoTight->GetNbinsX(); //std::cout<<elpt<<std::endl;
+                if( ybin == 0 ) std::cerr<<"Invalid eta stored for a good electron!"<<std::endl;
+
+                for( unsigned int iptbin = 0; iptbin < eleSFHistoReco->GetNbinsY()+1; iptbin++ ) {
+                    double tempPtBinEdgeMax = (double) eleSFHistoReco->GetYaxis()->GetBinUpEdge(iptbin);
+                    if( elpt < tempPtBinEdgeMax ) {
+                        ptbin = iptbin;
+                        break;
+                    }
+                }
+
+                for( unsigned int ietabin = 0; ietabin < eleSFHistoReco->GetNbinsX()+1; ietabin++ ) {
+                    double tempEtaBinEdgeMax = (double) eleSFHistoReco->GetXaxis()->GetBinUpEdge(ietabin);
+                    if( eleta < tempEtaBinEdgeMax ) {
+                        etabin = ietabin;
+                        break;
+                    }
+                }
+                
+                if( ptbin == 0 && elpt > 500.0) ptbin = eleSFHistoReco->GetNbinsY(); //std::cout<<elpt<<std::endl;
+                if( etabin == 0 ) std::cerr<<"Invalid eta stored for a good electron!"<<std::endl;
+
+
+                if( xbin != 0 && ybin != 0 && ptbin != 0 && etabin != 0 ) {
+                    double eleTotSF = eleSFHistoTight->GetBinContent( xbin, ybin ) * eleSFHistoIso->GetBinContent( xbin,ybin ) * eleSFHistoReco->GetBinContent( etabin, ptbin );
+                    if( eleTotSF < 0.1 ) {
+                        std::cout<<"EL pt: "<<elpt<<"; eta:"<<eleta<<"; "<<xbin<<" "<<ybin<<" "<<etabin<<" "<<ptbin<<" "<<eleSFHistoTight->GetBinContent(xbin,ybin)<<" "<<eleSFHistoIso->GetBinContent(xbin,ybin)<<" "<<eleSFHistoReco->GetBinContent(etabin, ptbin)<<std::endl;
+                    }
+                    electronSFs->push_back(eleTotSF);
+                    
+                    //If leadingElectronSF is -1.0, then set it to the first electron SF obtained (ranked by pT)
+                    if( leadingElectronSF < 0.0) {
+                        leadingElectronSF = eleTotSF;
+                    }
+                }
+            }
+        }
+
+        tr.registerDerivedVar( "leadGoodElectronSF", leadingElectronSF );
+        tr.registerDerivedVec( "electronSFs", electronSFs );
+
+        //Adding code for implementing muon scale factors
+        TFile muSFRootFile( muSFRootFileName_.c_str() );
+
+        TH2F *muSFHisto     = (TH2F*)muSFRootFile.Get("sf_mu_mediumID_mini02");
+        TGraph *muSFHistoReco = (TGraph*)muSFRootFile.Get("ratio_eff_aeta_dr030e030_corr");
+
+        const auto& muons           = tr.getVec<TLorentzVector>("Muons");
+        const auto& goodMuons       = tr.getVec<bool>("GoodMuons");
+
+        auto* muonSFs               = new std::vector<double>;
+        double leadingMuonSF = -1.0;
+
+        //Loop through the muons
+        for( unsigned int imu = 0; imu < muons.size(); imu++ ) {
+            //If it is not a good lepton, give scale factor of 1.0
+            if( !goodMuons.at(imu) ) muonSFs->push_back(1.0);
+
+            else {
+                //Get the scale factor from the rootfile
+                double mupt = muons.at(imu).Pt();
+                double mueta = std::fabs( muons.at(imu).Eta() );
+                
+                int xbin = 0, ybin = 0, etabin = 0;
+
+                for( unsigned int ixbin = 0; ixbin < muSFHisto->GetNbinsX()+1; ixbin++ ) {
+                    double tempxBinEdgeMax = muSFHisto->GetXaxis()->GetBinUpEdge(ixbin);
+                    if( mupt < tempxBinEdgeMax )  {
+                        xbin = ixbin; 
+                        break;
+                    }
+                }
+
+                for( unsigned int iybin = 0; iybin < muSFHisto->GetNbinsY()+1; iybin++ ) {
+                    double tempyBinEdgeMax = muSFHisto->GetYaxis()->GetBinUpEdge(iybin);
+                    if( mueta < tempyBinEdgeMax ) {
+                        ybin = iybin;
+                        break;
+                    }
+                }
+                
+                
+                if( xbin == 0 && mupt > 200.0) xbin = muSFHisto->GetNbinsX(); //std::cout<<elpt<<std::endl;
+                if( ybin == 0 ) std::cerr<<"Invalid eta stored for a good muon!"<<std::endl;
+                
+                //std::cout<<"MU pt: "<<mupt<<" ; eta: "<<mueta<<"; "<<xbin<<" "<<ybin<<" "<<muSFHisto->GetBinContent(xbin,ybin)<<" "<<muSFHistoReco->Eval(mueta)<<";"<<muSFHisto->GetBinContent(xbin,ybin)*muSFHistoReco->Eval(mueta)<<std::endl;
+
+                if( xbin != 0 && ybin != 0) {
+
+                    double muTotSF = muSFHisto->GetBinContent( xbin, ybin ) * muSFHistoReco->Eval( mueta );
+                    std::cout<<muTotSF<<std::endl;
+                    muonSFs->push_back(muTotSF);
+                    
+                    //If leadingElectronSF is -1.0, then set it to the first electron SF obtained (ranked by pT)
+                    if( leadingMuonSF < 0.0) {
+                        leadingMuonSF = muTotSF;
+                    }
+                }
+            }
+        }
+        
+        tr.registerDerivedVar( "leadGoodMuonSF", leadingMuonSF );
+        tr.registerDerivedVec( "muonSFs", muonSFs );
     }
 
 public:
-    ScaleFactors()
+    ScaleFactors( std::string eleSFRootFileName = "ElectronScaleFactors_Run2016.root", std::string muSFRootFileName = "allINone_leptonSF_Moriond17.root")
+        : eleSFRootFileName_(eleSFRootFileName)
+        , muSFRootFileName_(muSFRootFileName)
     {}
 
     void operator()(NTupleReader& tr)
