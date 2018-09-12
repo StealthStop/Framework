6b0c114ecca7815250084b42a3ec573dedbfed24
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-09-12 17:50:38 -0500

    Added deepESM binEdge value to the config file

--- Framework/include/DeepEventShape.h
+++ Framework/include/DeepEventShape.h
@@ -17,8 +17,8 @@ private:
 
     int fwm2_top6_, fwm3_top6_, fwm4_top6_, fwm5_top6_, fwm6_top6_, fwm7_top6_, fwm8_top6_, fwm9_top6_, fwm10_top6_, jmt_ev0_top6_, jmt_ev1_top6_, jmt_ev2_top6_;
     int NGoodJets_double_;
-    int Jet_pt_1_, Jet_pt_2_, Jet_pt_3_, Jet_pt_4_, Jet_pt_5_, Jet_pt_6_, Jet_eta_1_, Jet_eta_2_, Jet_eta_3_, Jet_eta_4_, Jet_eta_5_, Jet_eta_6_;
-    int Jet_phi_1_, Jet_phi_2_, Jet_phi_3_, Jet_phi_4_, Jet_phi_5_, Jet_phi_6_, Jet_m_1_, Jet_m_2_, Jet_m_3_, Jet_m_4_, Jet_m_5_, Jet_m_6_; 
+    int Jet_pt_1_, Jet_pt_2_, Jet_pt_3_, Jet_pt_4_, Jet_pt_5_, Jet_pt_6_, Jet_pt_7_, Jet_eta_1_, Jet_eta_2_, Jet_eta_3_, Jet_eta_4_, Jet_eta_5_, Jet_eta_6_, Jet_eta_7_;
+    int Jet_phi_1_, Jet_phi_2_, Jet_phi_3_, Jet_phi_4_, Jet_phi_5_, Jet_phi_6_, Jet_phi_7_, Jet_m_1_, Jet_m_2_, Jet_m_3_, Jet_m_4_, Jet_m_5_, Jet_m_6_, Jet_m_7_; 
     int BestComboAvgMass_;
 
 public:
@@ -26,8 +26,8 @@ public:
     {
         fwm2_top6_ = fwm3_top6_ = fwm4_top6_ = fwm5_top6_ = fwm6_top6_ = fwm7_top6_ = fwm8_top6_ = fwm9_top6_ = fwm10_top6_ = jmt_ev0_top6_ = jmt_ev1_top6_ = jmt_ev2_top6_ = -1; 
         NGoodJets_double_ = -1;
-        Jet_pt_1_ = Jet_pt_2_ = Jet_pt_3_ = Jet_pt_4_ = Jet_pt_5_ = Jet_pt_6_ = Jet_eta_1_ = Jet_eta_2_ = Jet_eta_3_ = Jet_eta_4_ = Jet_eta_5_ = Jet_eta_6_ = -1;
-        Jet_phi_1_ = Jet_phi_2_ = Jet_phi_3_ = Jet_phi_4_ = Jet_phi_5_ = Jet_phi_6_ = Jet_m_1_ = Jet_m_2_ = Jet_m_3_ = Jet_m_4_ = Jet_m_5_ = Jet_m_6_ = -1; 
+        Jet_pt_1_ = Jet_pt_2_ = Jet_pt_3_ = Jet_pt_4_ = Jet_pt_5_ = Jet_pt_6_ = Jet_pt_7_ = Jet_eta_1_ = Jet_eta_2_ = Jet_eta_3_ = Jet_eta_4_ = Jet_eta_5_ = Jet_eta_6_ = Jet_eta_7_= -1;
+        Jet_phi_1_ = Jet_phi_2_ = Jet_phi_3_ = Jet_phi_4_ = Jet_phi_5_ = Jet_phi_6_ = Jet_phi_7_= Jet_m_1_ = Jet_m_2_ = Jet_m_3_ = Jet_m_4_ = Jet_m_5_ = Jet_m_6_ = Jet_m_7_ = -1; 
         BestComboAvgMass_ = -1;
     }
 
@@ -59,24 +59,28 @@ public:
             else if(vars[i].compare("Jet_pt_4") == 0) Jet_pt_4_ = i;
             else if(vars[i].compare("Jet_pt_5") == 0) Jet_pt_5_ = i;
             else if(vars[i].compare("Jet_pt_6") == 0) Jet_pt_6_ = i;
+            else if(vars[i].compare("Jet_pt_7") == 0) Jet_pt_7_ = i;
             else if(vars[i].compare("Jet_eta_1") == 0) Jet_eta_1_ = i;
             else if(vars[i].compare("Jet_eta_2") == 0) Jet_eta_2_ = i;
             else if(vars[i].compare("Jet_eta_3") == 0) Jet_eta_3_ = i;
             else if(vars[i].compare("Jet_eta_4") == 0) Jet_eta_4_ = i;
             else if(vars[i].compare("Jet_eta_5") == 0) Jet_eta_5_ = i;
             else if(vars[i].compare("Jet_eta_6") == 0) Jet_eta_6_ = i;
+            else if(vars[i].compare("Jet_eta_7") == 0) Jet_eta_7_ = i;
             else if(vars[i].compare("Jet_phi_1") == 0) Jet_phi_1_ = i;
             else if(vars[i].compare("Jet_phi_2") == 0) Jet_phi_2_ = i;
             else if(vars[i].compare("Jet_phi_3") == 0) Jet_phi_3_ = i;
             else if(vars[i].compare("Jet_phi_4") == 0) Jet_phi_4_ = i;
             else if(vars[i].compare("Jet_phi_5") == 0) Jet_phi_5_ = i;
             else if(vars[i].compare("Jet_phi_6") == 0) Jet_phi_6_ = i;
+            else if(vars[i].compare("Jet_phi_7") == 0) Jet_phi_7_ = i;
             else if(vars[i].compare("Jet_m_1") == 0) Jet_m_1_ = i;
             else if(vars[i].compare("Jet_m_2") == 0) Jet_m_2_ = i;
             else if(vars[i].compare("Jet_m_3") == 0) Jet_m_3_ = i;
             else if(vars[i].compare("Jet_m_4") == 0) Jet_m_4_ = i;
             else if(vars[i].compare("Jet_m_5") == 0) Jet_m_5_ = i;
             else if(vars[i].compare("Jet_m_6") == 0) Jet_m_6_ = i;
+            else if(vars[i].compare("Jet_m_7") == 0) Jet_m_7_ = i;
             else if(vars[i].compare("BestComboAvgMass") == 0) BestComboAvgMass_ = i;
         }
     }
@@ -108,24 +112,28 @@ public:
         if(Jet_pt_4_ >= 0) *(basePtr_ + Jet_pt_4_ + len_*iCand) = tr.getVar<double>("Jet_pt_4");
         if(Jet_pt_5_ >= 0) *(basePtr_ + Jet_pt_5_ + len_*iCand) = tr.getVar<double>("Jet_pt_5");
         if(Jet_pt_6_ >= 0) *(basePtr_ + Jet_pt_6_ + len_*iCand) = tr.getVar<double>("Jet_pt_6");
+        if(Jet_pt_7_ >= 0) *(basePtr_ + Jet_pt_7_ + len_*iCand) = tr.getVar<double>("Jet_pt_7");
         if(Jet_eta_1_ >= 0) *(basePtr_ + Jet_eta_1_ + len_*iCand) = tr.getVar<double>("Jet_eta_1");
         if(Jet_eta_2_ >= 0) *(basePtr_ + Jet_eta_2_ + len_*iCand) = tr.getVar<double>("Jet_eta_2");
         if(Jet_eta_3_ >= 0) *(basePtr_ + Jet_eta_3_ + len_*iCand) = tr.getVar<double>("Jet_eta_3");
         if(Jet_eta_4_ >= 0) *(basePtr_ + Jet_eta_4_ + len_*iCand) = tr.getVar<double>("Jet_eta_4");
         if(Jet_eta_5_ >= 0) *(basePtr_ + Jet_eta_5_ + len_*iCand) = tr.getVar<double>("Jet_eta_5");
         if(Jet_eta_6_ >= 0) *(basePtr_ + Jet_eta_6_ + len_*iCand) = tr.getVar<double>("Jet_eta_6");
+        if(Jet_eta_7_ >= 0) *(basePtr_ + Jet_eta_7_ + len_*iCand) = tr.getVar<double>("Jet_eta_7");
         if(Jet_phi_1_ >= 0) *(basePtr_ + Jet_phi_1_ + len_*iCand) = tr.getVar<double>("Jet_phi_1");
         if(Jet_phi_2_ >= 0) *(basePtr_ + Jet_phi_2_ + len_*iCand) = tr.getVar<double>("Jet_phi_2");
         if(Jet_phi_3_ >= 0) *(basePtr_ + Jet_phi_3_ + len_*iCand) = tr.getVar<double>("Jet_phi_3");
         if(Jet_phi_4_ >= 0) *(basePtr_ + Jet_phi_4_ + len_*iCand) = tr.getVar<double>("Jet_phi_4");
         if(Jet_phi_5_ >= 0) *(basePtr_ + Jet_phi_5_ + len_*iCand) = tr.getVar<double>("Jet_phi_5");
         if(Jet_phi_6_ >= 0) *(basePtr_ + Jet_phi_6_ + len_*iCand) = tr.getVar<double>("Jet_phi_6");
+        if(Jet_phi_7_ >= 0) *(basePtr_ + Jet_phi_7_ + len_*iCand) = tr.getVar<double>("Jet_phi_7");
         if(Jet_m_1_ >= 0) *(basePtr_ + Jet_m_1_ + len_*iCand) = tr.getVar<double>("Jet_m_1");
         if(Jet_m_2_ >= 0) *(basePtr_ + Jet_m_2_ + len_*iCand) = tr.getVar<double>("Jet_m_2");
         if(Jet_m_3_ >= 0) *(basePtr_ + Jet_m_3_ + len_*iCand) = tr.getVar<double>("Jet_m_3");
         if(Jet_m_4_ >= 0) *(basePtr_ + Jet_m_4_ + len_*iCand) = tr.getVar<double>("Jet_m_4");
         if(Jet_m_5_ >= 0) *(basePtr_ + Jet_m_5_ + len_*iCand) = tr.getVar<double>("Jet_m_5");
         if(Jet_m_6_ >= 0) *(basePtr_ + Jet_m_6_ + len_*iCand) = tr.getVar<double>("Jet_m_6");
+        if(Jet_m_7_ >= 0) *(basePtr_ + Jet_m_7_ + len_*iCand) = tr.getVar<double>("Jet_m_7");
         if(BestComboAvgMass_ >= 0) *(basePtr_ + BestComboAvgMass_ + len_*iCand) = tr.getVar<double>("BestComboAvgMass");
     }
 };
@@ -141,6 +149,7 @@ private:
 
     //Input variable names 
     std::vector<std::string> vars_;
+    std::vector<double> binEdges_;
 
     std::vector<TF_Output>     inputs_;
     std::vector<TF_Output>     outputs_;
@@ -148,6 +157,32 @@ private:
 
     //variable calclator
     std::shared_ptr<EventShapeCalculator> varCalculator_;
+
+    template<typename T> std::vector<T> getVecFromCfg(const std::unique_ptr<cfg::CfgDocument>& cfgDoc, const std::string& var, const cfg::Context& localCxt, const T& defaultYo)
+    {
+        std::vector<T> vec;
+        int iVar = 0;
+        bool keepLooping;
+        do
+        {
+            keepLooping = false;
+        
+            //Get variable name
+            T v = cfgDoc->get(var, iVar, localCxt, defaultYo);
+        
+            //if it is a non empty string save in vector
+            if(v != defaultYo)
+            {
+                keepLooping = true;
+        
+                vec.push_back(v);
+            }
+            ++iVar;
+        }
+        while(keepLooping);
+        
+        return vec;
+    }
 
     void getParameters(const std::unique_ptr<cfg::CfgDocument>& cfgDoc, const std::string& localContextName)
     {
@@ -157,27 +192,8 @@ private:
         modelFile_     = cfgDoc->get("modelFile", localCxt, "");
         inputOp_       = cfgDoc->get("inputOp",   localCxt, "x");
         outputOp_      = cfgDoc->get("outputOp",  localCxt, "y");
-
-        int iVar = 0;
-        bool keepLooping;
-        do
-        {
-            keepLooping = false;
-        
-            //Get variable name
-            std::string varName = cfgDoc->get("mvaVar", iVar, localCxt, "");
-        
-            //if it is a non empty string save in vector
-            if(varName.size() > 0)
-            {
-                keepLooping = true;
-        
-                vars_.push_back(varName);
-                std::cout<<varName<<std::endl;
-            }
-            ++iVar;
-        }
-        while(keepLooping);
+        vars_          = getVecFromCfg<std::string>(cfgDoc, "mvaVar", localCxt, "");
+        binEdges_      = getVecFromCfg<double>(cfgDoc, "binEdges", localCxt, -1);
         
         //Variable to hold tensorflow status
         TF_Status* status = TF_NewStatus();
@@ -264,21 +280,14 @@ private:
         
         TF_DeleteStatus(status);
 
-        //Define bins (try to have the same amount of events in all 4 bins)
-        //double bin12 = 0.4075, bin23 = 0.4375, bin34 = 0.4875; //24Vars_GRTrue
-        double bin12 = 0.4525, bin23 = 0.4725, bin34 = 0.5075; //24Vars_GRTrue_v2
-
-        bool deepESM_bin1 = discriminator > 0.000 && discriminator <= bin12;
-        bool deepESM_bin2 = discriminator > bin12 && discriminator <= bin23;
-        bool deepESM_bin3 = discriminator > bin23 && discriminator <= bin34;
-        bool deepESM_bin4 = discriminator > bin34 && discriminator <= 1.000;
-        
         // Register Variables
         tr.registerDerivedVar("deepESM_val", discriminator);
-        tr.registerDerivedVar("deepESM_bin1", deepESM_bin1);
-        tr.registerDerivedVar("deepESM_bin2", deepESM_bin2);
-        tr.registerDerivedVar("deepESM_bin3", deepESM_bin3);
-        tr.registerDerivedVar("deepESM_bin4", deepESM_bin4);
+        // Define and register deepESM bins
+        for(int i = 1; i < binEdges_.size(); i++)
+        {
+            bool passDeepESMBin = discriminator > binEdges_[i-1] && discriminator <= binEdges_[i];
+            tr.registerDerivedVar("deepESM_bin"+std::to_string(i), passDeepESMBin);
+        }
     }
 
     static void free_buffer(void* data, size_t length) 
