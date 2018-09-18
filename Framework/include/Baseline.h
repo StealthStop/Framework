d547bc7185bcb91cd7d722165624124de3c5c742
Nadja <nadja.strobbe@cern.ch>
2018-09-18 15:54:06 -0500

    remove HT overlap for MG ttbar samples

--- Framework/include/Baseline.h
+++ Framework/include/Baseline.h
@@ -34,9 +34,12 @@ private:
         if(runtype == "MC")
         {
             const auto& madHT  = tr.getVar<double>("madHT");
-            // Exclude events with MadGraph HT > 100 from the DY inclusive sample
+            // Exclude events with MadGraph HT > 100 from the DY & WJets inclusive samples
             if(filetag == "DYJetsToLL_M-50_Incl" && madHT > 100) passMadHT = false;
             if(filetag == "WJetsToLNu_Incl" && madHT > 100) passMadHT = false;
+            // Stitch TTbar samples together
+            if( (filetag == "TTJets_Incl" || filetag == "TTJets_SingleLeptFromT" || filetag == "TTJets_SingleLeptFromTbar" || filetag == "TTJets_DiLept") 
+                && madHT > 600) passMadHT = false;
         }
         
         // ------------------------------
