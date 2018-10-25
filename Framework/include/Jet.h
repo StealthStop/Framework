e795f5c4767bdadba1fdf7fb7effea3e384243a8
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-10-25 13:45:05 -0500

    Adding a suffix to reg. variables when running JEC

--- Framework/include/Jet.h
+++ Framework/include/Jet.h
@@ -116,22 +116,22 @@ private:
             setJetVar( abs(lv.Eta()) < etaCut && lv.Pt() > 45 && goodjets_->at(i),    goodjets_pt45_, NGoodJets_pt45);
         }
 
-        tr.registerDerivedVar("NJets",       static_cast<int>(Jets.size()));
-        tr.registerDerivedVec("Jets_pt30",   jets_pt30_);
-        tr.registerDerivedVar("NJets_pt30",  NJets_pt30);
-        tr.registerDerivedVec("Jets_pt40",   jets_pt40_);
-        tr.registerDerivedVar("NJets_pt40",  NJets_pt40);
-        tr.registerDerivedVec("Jets_pt45",   jets_pt45_);
-        tr.registerDerivedVar("NJets_pt45",  NJets_pt45);
+        tr.registerDerivedVar("NJets"     +myVarSuffix_,  static_cast<int>(Jets.size()));
+        tr.registerDerivedVec("Jets_pt30" +myVarSuffix_,  jets_pt30_);
+        tr.registerDerivedVar("NJets_pt30"+myVarSuffix_,  NJets_pt30);
+        tr.registerDerivedVec("Jets_pt40" +myVarSuffix_,  jets_pt40_);
+        tr.registerDerivedVar("NJets_pt40"+myVarSuffix_,  NJets_pt40);
+        tr.registerDerivedVec("Jets_pt45" +myVarSuffix_,  jets_pt45_);
+        tr.registerDerivedVar("NJets_pt45"+myVarSuffix_,  NJets_pt45);
         
-        tr.registerDerivedVec("GoodJets",       goodjets_);
-        tr.registerDerivedVar("NGoodJets",      NGoodJets);
-        tr.registerDerivedVec("GoodJets_pt30",  goodjets_pt30_);
-        tr.registerDerivedVar("NGoodJets_pt30", NGoodJets_pt30);
-        tr.registerDerivedVec("GoodJets_pt40",  goodjets_pt40_);
-        tr.registerDerivedVar("NGoodJets_pt40", NGoodJets_pt40);
-        tr.registerDerivedVec("GoodJets_pt45",  goodjets_pt45_);
-        tr.registerDerivedVar("NGoodJets_pt45", NGoodJets_pt45);
+        tr.registerDerivedVec("GoodJets"      +myVarSuffix_, goodjets_);
+        tr.registerDerivedVar("NGoodJets"     +myVarSuffix_, NGoodJets);
+        tr.registerDerivedVec("GoodJets_pt30" +myVarSuffix_, goodjets_pt30_);
+        tr.registerDerivedVar("NGoodJets_pt30"+myVarSuffix_, NGoodJets_pt30);
+        tr.registerDerivedVec("GoodJets_pt40" +myVarSuffix_, goodjets_pt40_);
+        tr.registerDerivedVar("NGoodJets_pt40"+myVarSuffix_, NGoodJets_pt40);
+        tr.registerDerivedVec("GoodJets_pt45" +myVarSuffix_, goodjets_pt45_);
+        tr.registerDerivedVar("NGoodJets_pt45"+myVarSuffix_, NGoodJets_pt45);
     }
 
 public:
