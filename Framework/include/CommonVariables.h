19ec0f606bfb9a8df1f56809644566bf53aac313
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2019-09-11 17:34:03 -0500

    Forgot to register the new bool

--- Framework/include/CommonVariables.h
+++ Framework/include/CommonVariables.h
@@ -88,6 +88,7 @@ private:
         bool passHEMVeto = (!objectInHEM(Jets,      -3.20, -1.10, -1.77, -0.67, 20.0, runYear)) && 
                            (!objectInHEM(Muons,     -3.00, -1.30, -1.57, -0.87, 20.0, runYear)) && 
                            (!objectInHEM(Electrons, -3.00, -1.30, -1.57, -0.87, 20.0, runYear));
+        tr.registerDerivedVar("passHEMVeto"+myVarSuffix_, passHEMVeto);
 
         // HT of jets with pT>40
         double ht = 0;
