1173659adf63c2bdb9b664561951e14b1d19fd53
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2019-10-21 12:39:47 -0500

    Now registering TLorentzVectors for Mega Jets; added scalar ranck Pt vars.

--- Framework/include/MakeMT2Hemispheres.h
+++ Framework/include/MakeMT2Hemispheres.h
@@ -12,31 +12,40 @@ class MakeMT2Hemispheres
 private:
     std::string jetMaskName_, nJetName_, myVarSuffix_;
 
+    template<typename T> void orderVars(T& stop1, T& stop2, const T pseudo1, const T pseudo2, const bool pseudo1Tostop1) const
+    {
+        if(pseudo1Tostop1) 
+        {            
+            stop1 = pseudo1;
+            stop2 = pseudo2;      
+        } 
+        else
+        {
+            stop1 = pseudo2;
+            stop2 = pseudo1;      
+        }
+    }
+
     void getHemispheres(NTupleReader& tr) const
     {
-        // these variables values the same as  MT2
-        const int hemi_association = 3; // 3: 3th method, 'lund' used by MT2  
+        const auto& met                = tr.getVar<double>("MET");
+        const auto& metPhi             = tr.getVar<double>("METPhi");
+        const auto& Jets               = tr.getVec<TLorentzVector>("Jets");
+        const auto& GoodJets           = tr.getVec<bool>(jetMaskName_);
+        const auto& NGoodJets          = tr.getVar<int>(nJetName_);
+        const auto& GoodLeptons        = tr.getVec<std::pair<std::string, TLorentzVector>>("GoodLeptons");
 
-        const auto& met                         = tr.getVar<double>("MET");
-        const auto& metPhi                      = tr.getVar<double>("METPhi");
-        const auto& Jets                        = tr.getVec<TLorentzVector>("Jets");
-        const auto& GoodJets                    = tr.getVec<bool>(jetMaskName_);
-        const auto& NGoodJets                   = tr.getVar<int>(nJetName_);
-        const auto& GoodLeptons                 = tr.getVec<std::pair<std::string, TLorentzVector>>("GoodLeptons");
-        double MT2                              = 0.0;
-        double stop1Mass                        = -9999.9, stop1Eta = -9999.9, stop1Phi = -9999.9, stop1Pt = -9999.9;
-        double stop2Mass                        = -9999.9, stop2Eta = -9999.9, stop2Phi = -9999.9, stop2Pt = -9999.9;
-        double dR_stop1stop2                    = -1;
-        double dPhi_stop1stop2                  = -1;
-        double stop1Mass_PtRank                 = -9999.9, stop2Mass_PtRank   = -9999.9; 
-        double stop1Mass_MassRank               = -9999.9, stop2Mass_MassRank = -9999.9;
-        double difference_stopMasses_PtRank     = -9999.9;
-        double relativeDiff_stopMasses_PtRank   = -9999.9;
-        double difference_stopMasses_MassRank   = -9999.9;
-        double relativeDiff_stopMasses_MassRank = -9999.9;        
-        double difference_stopMasses            = -9999.9;
-        double average_stopMasses               = -9999.9;
-        double relativeDiff_stopMasses          = -9999.9;
+        static const int hemi_association = 3; // 3: 3th method, 'lund' used by MT2  
+        TLorentzVector stop1_PtRank,       stop2_PtRank;
+        TLorentzVector stop1_MassRank,     stop2_MassRank;
+        TLorentzVector stop1_ScalarPtRank, stop2_ScalarPtRank;
+        double stop1ScalarPt_ScalarPtRank = -9999.9, stop2ScalarPt_ScalarPtRank = -9999.9;
+        double MT2                        = 0.0;
+        double dR_stop1stop2              = -1;
+        double dPhi_stop1stop2            = -1;
+        double difference_stopMasses      = -9999.9;
+        double average_stopMasses         = -9999.9;
+        double relativeDiff_stopMasses    = -9999.9;
  
         if(NGoodJets >= 2)
         {
@@ -61,102 +70,62 @@ private:
             }
 
             // Get hemispheres (seed 2: max inv mass, association method: default 3 = minimal lund distance)  
+            asymm_mt2_lester_bisect::disableCopyrightMessage();
             Hemisphere hemi(px, py, pz, E, 2, hemi_association);
             vector<int> grouping = hemi.getGrouping();
-            TLorentzVector pseudojet1(0.0, 0.0, 0.0, 0.0);
-            TLorentzVector pseudojet2(0.0, 0.0, 0.0, 0.0);
+            TLorentzVector pseudojet1, pseudojet2;
+            double pseudojet1ScalarPt = 0.0, pseudojet2ScalarPt = 0.0;
 
+            // Perform vector and scalar sum of mega jets
             for(int i=0; i < px.size(); ++i)
             {
+                TLorentzVector obj;
+                obj.SetPxPyPzE(px[i], py[i], pz[i], E[i]);
+
                 if(grouping[i] == 1)
                 {
-                    pseudojet1.SetPx(pseudojet1.Px() + px[i]);
-                    pseudojet1.SetPy(pseudojet1.Py() + py[i]);
-                    pseudojet1.SetPz(pseudojet1.Pz() + pz[i]);
-                    pseudojet1.SetE( pseudojet1.E()  + E[i]);
-                }            
+                    //vector sum
+                    pseudojet1 += obj;
+
+                    //scalar sum
+                    pseudojet1ScalarPt += obj.Pt();
+                }
                 else if(grouping[i] == 2)
                 {
-                    pseudojet2.SetPx(pseudojet2.Px() + px[i]);
-                    pseudojet2.SetPy(pseudojet2.Py() + py[i]);
-                    pseudojet2.SetPz(pseudojet2.Pz() + pz[i]);
-                    pseudojet2.SetE( pseudojet2.E()  + E[i]);
+                    //vector sum
+                    pseudojet2 += obj;
+
+                    //scalar sum
+                    pseudojet2ScalarPt += obj.Pt();
                 }
             }
-                
-            // --------------------------
-            // -- stop MT2 stopspheres 
-            // --------------------------
-            MT2             = ttUtility::coreMT2calc(pseudojet1, pseudojet2, MET);
-            stop1Mass       = pseudojet1.M();
-            stop1Eta        = pseudojet1.Eta();
-            stop1Phi        = pseudojet1.Phi();
-            stop1Pt         = pseudojet1.Pt();
-            stop2Mass       = pseudojet2.M();
-            stop2Eta        = pseudojet2.Eta();
-            stop2Phi        = pseudojet2.Phi();
-            stop2Pt         = pseudojet2.Pt(); 
-            dR_stop1stop2   = pseudojet1.DeltaR(pseudojet2);    
-            dPhi_stop1stop2 = pseudojet1.DeltaPhi(pseudojet2);
-            
-            // stopMass Pt rank
-            if (pseudojet1.Pt() > pseudojet2.Pt()) 
-            {
-                stop1Mass_PtRank               = pseudojet1.M();
-                stop2Mass_PtRank               = pseudojet2.M();      
-                difference_stopMasses_PtRank   = ( pseudojet1.M() - pseudojet2.M() );
-                relativeDiff_stopMasses_PtRank = ( pseudojet1.M() - pseudojet2.M() ) / ( 0.5 * ( pseudojet1.M() + pseudojet2.M() ) );     
-            } 
-            else
-            {
-                stop1Mass_PtRank               = pseudojet2.M();
-                stop2Mass_PtRank               = pseudojet1.M();
-                difference_stopMasses_PtRank   = ( pseudojet2.M() - pseudojet1.M() );
-                relativeDiff_stopMasses_PtRank = ( pseudojet2.M() - pseudojet1.M() ) / ( 0.5 * ( pseudojet1.M() + pseudojet2.M() ) );
 
-            }
-    
-            // stopMass Mass rank
-            if (pseudojet1.M() > pseudojet2.M())
-            {
-                stop1Mass_MassRank               = pseudojet1.M();
-                stop2Mass_MassRank               = pseudojet2.M();
-                difference_stopMasses_MassRank   = ( pseudojet1.M() - pseudojet2.M() );
-                relativeDiff_stopMasses_MassRank = ( pseudojet1.M() - pseudojet2.M() ) / ( 0.5 * ( pseudojet1.M() + pseudojet2.M() ) );
-            } 
-            else
-            {
-                stop1Mass_MassRank               = pseudojet2.M();
-                stop2Mass_MassRank               = pseudojet1.M();
-                difference_stopMasses_MassRank   = ( pseudojet2.M() - pseudojet1.M() );
-                relativeDiff_stopMasses_MassRank = ( pseudojet2.M() - pseudojet1.M() ) / ( 0.5 * ( pseudojet1.M() + pseudojet2.M() ) );
-            }
-        
-            // stop Masses difference & average & relative difference
-            difference_stopMasses   = ( pseudojet1.M() - pseudojet2.M() );
+            // ---------------------------------------
+            // -- Rank mega jets (Mass, Pt, Scalar Pt) 
+            // ---------------------------------------
+            orderVars(stop1_PtRank,               stop2_PtRank,               pseudojet1,         pseudojet2,         pseudojet1.Pt()    > pseudojet2.Pt()   );
+            orderVars(stop1_MassRank,             stop2_MassRank,             pseudojet1,         pseudojet2,         pseudojet1.M()     > pseudojet2.M()    );
+            orderVars(stop1_ScalarPtRank,         stop2_ScalarPtRank,         pseudojet1,         pseudojet2,         pseudojet1ScalarPt > pseudojet2ScalarPt);
+            orderVars(stop1ScalarPt_ScalarPtRank, stop2ScalarPt_ScalarPtRank, pseudojet1ScalarPt, pseudojet2ScalarPt, pseudojet1ScalarPt > pseudojet2ScalarPt);                
+
+            MT2                     = ttUtility::coreMT2calc(pseudojet1, pseudojet2, MET);
+            dR_stop1stop2           = pseudojet1.DeltaR(pseudojet2);    
+            dPhi_stop1stop2         = pseudojet1.DeltaPhi(pseudojet2);
+            difference_stopMasses   = abs( pseudojet1.M() - pseudojet2.M() );
             average_stopMasses      = ( 0.5 * ( pseudojet1.M() + pseudojet2.M() ) );
-            relativeDiff_stopMasses = ( pseudojet1.M() - pseudojet2.M() ) / ( 0.5 * ( pseudojet1.M() + pseudojet2.M() ) );
-
+            relativeDiff_stopMasses = difference_stopMasses / average_stopMasses;
         }
+        tr.registerDerivedVar("stop1_PtRank"+myVarSuffix_,stop1_PtRank);
+        tr.registerDerivedVar("stop2_PtRank"+myVarSuffix_,stop2_PtRank);
+        tr.registerDerivedVar("stop1_MassRank"+myVarSuffix_,stop1_MassRank);
+        tr.registerDerivedVar("stop2_MassRank"+myVarSuffix_,stop2_MassRank);
+        tr.registerDerivedVar("stop1_ScalarPtRank"+myVarSuffix_,stop1_ScalarPtRank);
+        tr.registerDerivedVar("stop2_ScalarPtRank"+myVarSuffix_,stop2_ScalarPtRank);
+        tr.registerDerivedVar("stop1ScalarPt_ScalarPtRank"+myVarSuffix_,stop1_ScalarPtRank);
+        tr.registerDerivedVar("stop2ScalarPt_ScalarPtRank"+myVarSuffix_,stop2_ScalarPtRank);        
         tr.registerDerivedVar("MT2"+myVarSuffix_,MT2);
-        tr.registerDerivedVar("stop1Mass"+myVarSuffix_,stop1Mass);
-        tr.registerDerivedVar("stop1Eta"+myVarSuffix_,stop1Eta);
-        tr.registerDerivedVar("stop1Phi"+myVarSuffix_,stop1Phi);
-        tr.registerDerivedVar("stop1Pt"+myVarSuffix_,stop1Pt);
-        tr.registerDerivedVar("stop2Mass"+myVarSuffix_,stop2Mass);
-        tr.registerDerivedVar("stop2Eta"+myVarSuffix_,stop2Eta);
-        tr.registerDerivedVar("stop2Phi"+myVarSuffix_,stop2Phi);
-        tr.registerDerivedVar("stop2Pt"+myVarSuffix_,stop2Pt);
         tr.registerDerivedVar("dR_stop1stop2"+myVarSuffix_,dR_stop1stop2);
         tr.registerDerivedVar("dPhi_stop1stop2"+myVarSuffix_,dPhi_stop1stop2);
-        tr.registerDerivedVar("stop1Mass_PtRank"+myVarSuffix_,stop1Mass_PtRank);
-        tr.registerDerivedVar("stop2Mass_PtRank"+myVarSuffix_,stop2Mass_PtRank);
-        tr.registerDerivedVar("stop1Mass_MassRank"+myVarSuffix_,stop1Mass_MassRank);
-        tr.registerDerivedVar("stop2Mass_MassRank"+myVarSuffix_,stop2Mass_MassRank);
-        tr.registerDerivedVar("difference_stopMasses_PtRank"+myVarSuffix_,difference_stopMasses_PtRank);
-        tr.registerDerivedVar("relativeDiff_stopMasses_PtRank"+myVarSuffix_,relativeDiff_stopMasses_PtRank);
-        tr.registerDerivedVar("difference_stopMasses_MassRank"+myVarSuffix_,difference_stopMasses_MassRank);
-        tr.registerDerivedVar("relativeDiff_stopMasses_MassRank"+myVarSuffix_,relativeDiff_stopMasses_MassRank);
         tr.registerDerivedVar("difference_stopMasses"+myVarSuffix_,difference_stopMasses);
         tr.registerDerivedVar("average_stopMasses"+myVarSuffix_,average_stopMasses);
         tr.registerDerivedVar("relativeDiff_stopMasses"+myVarSuffix_,relativeDiff_stopMasses);
