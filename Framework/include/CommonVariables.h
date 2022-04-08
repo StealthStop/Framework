9a8c2efc85ba079b589a0fff73ebeb67974080c1
Semra <Semra.Turkcapar@cern.ch>
2022-04-08 09:21:49 -0500

    updated for using UL and testing first time

--- Framework/include/CommonVariables.h
+++ Framework/include/CommonVariables.h
@@ -213,13 +213,13 @@ private:
         genMatch(tr);
         
         //Find single lepton for HistoContainer
-        utility::LorentzVector singleLepton;
+        TLorentzVector singleLepton;
         for(unsigned int i = 0; i < Muons.size(); ++i)
         {
             if(!GoodMuons[i]) continue;
             if(Muons[i].Pt() > 20)
             {
-                singleLepton = Muons[i];
+                singleLepton = utility::convertLV<TLorentzVector, utility::LorentzVector>(Muons[i]);
                 break;
             }
         }
@@ -228,7 +228,7 @@ private:
             if(!GoodElectrons[i]) continue;
             if(Electrons[i].Pt() > 20)
             {
-                singleLepton = Electrons[i];
+                singleLepton = utility::convertLV<TLorentzVector, utility::LorentzVector>(Electrons[i]);
                 break;
             }
         }
