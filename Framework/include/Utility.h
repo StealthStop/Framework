08b6140e57b67a8ec3941fbf9cb15bf9f39ba108
Josh Hiltbrand <hiltbran@umn.edu>
2022-10-26 14:40:04 -0500

    doing some cleaning

--- Framework/include/Utility.h
+++ Framework/include/Utility.h
@@ -1,12 +1,44 @@
 #ifndef Utility_h
 #define Utility_h
 
-#include "Framework/Framework/include/Vector3D.h"
 #include "TLorentzVector.h"
+#include "Math/Vector3D.h"
 #include "Math/Vector4D.h"
 #include "Math/VectorUtil.h"
 #include "Math/Boost.h"
 #include <cmath>
+
+namespace math {
+
+  /// spatial vector with cartesian internal representation
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<double> > XYZVectorD;
+  /// spatial vector with cylindrical internal representation using pseudorapidity
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::CylindricalEta3D<double> > RhoEtaPhiVectorD;
+  /// spatial vector with polar internal representation
+  /// WARNING: ROOT dictionary not provided for the type below
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Polar3D<double> > RThetaPhiVectorD;
+
+
+  /// spatial vector with cartesian internal representation
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<float> > XYZVectorF;
+  /// spatial vector with cylindrical internal representation using pseudorapidity
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::CylindricalEta3D<float> > RhoEtaPhiVectorF;
+  /// spatial vector with polar internal representation
+  /// WARNING: ROOT dictionary not provided for the type below
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Polar3D<float> > RThetaPhiVectorF;
+
+  /// vector in local coordinate system
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<float>, ROOT::Math::LocalCoordinateSystemTag> LocalVector;
+  /// vector in glovbal coordinate system
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<float>, ROOT::Math::GlobalCoordinateSystemTag> GlobalVector;
+
+  /// spatial vector with cartesian internal representation
+  typedef XYZVectorD XYZVector;
+  /// spatial vector with cylindrical internal representation using pseudorapidity
+  typedef RhoEtaPhiVectorD RhoEtaPhiVector;
+  /// spatial vector with polar internal representation
+  typedef RThetaPhiVectorD RThetaPhiVector;
+}
 
 namespace utility
 {
